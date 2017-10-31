8629f06a352a0baed291ea6ca10bb06e533cff5e
Simon Mika <simon@mika.se>
2017-10-31 15:59:09 +0100

    Enabled rendering of future articles without them showing up in the listings.

--- tools/publish.sh
+++ tools/publish.sh
@@ -3,7 +3,7 @@ if [ ! -d "public/.git" ]; then
 	git clone --depth 1 --single-branch https://github.com/imintinvestors/imintinvestors.com -b gh-pages public
 fi
 rm -rf public/*
-hugo
+hugo -F
 pushd public
 git add -A
 git commit -m "$1"
